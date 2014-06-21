54081e182903c05647c61688943fa2c4339da360
Slavi <slavicvetanov@gmail.com>
2014-06-21 11:36:52 +0300

    Added disaster-info srcipt

---
+++ scripts/disaster-info.js
@@ -0,0 +1,30 @@
+(function (){
+    var info = [
+        {
+            eventType: 'Пожар',
+            eventInfo: [
+                {
+                    title: 'Основна информация',
+                    text: '',
+                    imageSource: 'images/fire.png'
+                },{
+                    title: 'Рискове',
+                    text: '',
+                    imageSource: 'images/fire-risk.png'
+                },{
+                    title: 'Какво да правим?',
+                    text: '',
+                    imageSource: 'images/fire-to-do.png'
+                }
+            ]
+        }
+    ];
+
+   //var disasters = ['Fire','Flood', 'Еpidemic', 'Heat Wave',
+   //    'Fire', 'Fire'  ]
+
+    function createSideList(){
+
+    }
+
+})();
