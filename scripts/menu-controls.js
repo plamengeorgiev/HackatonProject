719de6dcb030b78395515068224f67830673a8a9
soryusen <nick.ilarionov@gmail.com>
2014-06-21 15:23:28 +0300

    Merged Menu-Controls and Objects

---
+++ scripts/menu-controls.js
@@ -0,0 +1,142 @@
+﻿$(document).ready(function () {
+	console.log('started');
+	$(document).on('click', '#disasterButton', onDisastersBtnClick);
+
+	$(document).on('click', '#recycle', onRecycleBtnClick);
+	
+})
+
+function onDisastersBtnClick(){
+	console.log('Disaster Button Clicked');
+    var $wrapper = $('#wrapper');
+	$wrapper.empty();
+    var $sideMenu = createSideMenu(disastersInfoBG);
+    var $selectedDisaster = createEventContent(disastersInfoBG[0]);
+
+    $sideMenu.on('click', function(ev){
+			var $target = $(ev.target);
+			var $content = $(this).next();
+			$content.remove();
+
+			if ($target.hasClass('side-btn')){
+				var dataInfo = $target.attr('data-info');
+				dataInfo = parseInt(dataInfo);
+				console.log('reached; data info: ' + typeof(dataInfo));
+				console.log('reached; data info: ' + typeof(disastersInfoBG));
+				$('#wrapper').append(createEventContent(disastersInfoBG[dataInfo]));
+			}
+	});
+    $wrapper.append($sideMenu);
+    $wrapper.append($selectedDisaster);
+}
+
+function onRecycleBtnClick(){
+        var $wrapper = $('#wrapper');
+		$wrapper.empty();
+        var $sideMenu = createSideMenu(garbageBins);
+        var $selectedDisaster = createContent(garbageBins[0]);
+
+        $sideMenu.on('click', function(ev){
+			var $target = $(ev.target);
+			var $content = $(this).next();
+			$content.remove();
+
+			if ($target.hasClass('side-btn')){
+				var dataInfo = $target.attr('data-info');
+				dataInfo = parseInt(dataInfo);
+				console.log('reached; data info: ' + dataInfo);
+				$('#wrapper').append(createContent(garbageBins[dataInfo]));
+			}
+		});
+        $wrapper.append($sideMenu);
+        $wrapper.append($selectedDisaster);
+}
+
+function createSideMenu(array){
+    var $sideMenuDiv = $('<div/ >');
+	$sideMenuDiv.empty();
+    $sideMenuDiv.addClass('side-nav');
+
+    var $sideMenuList = $('<ul />');
+    $sideMenuList.addClass('nav-list');
+
+    for (var i = 0; i < array.length; i++) {
+        var $currMenuBtn = $button.clone();
+        $currMenuBtn.attr('data-info', i);
+		
+		//check which array is given
+		if(array[i].eventType === undefined || array[i].eventType === null){
+			$currMenuBtn.text(array[i].name);
+		} else {
+			$currMenuBtn.text(array[i].eventType);
+		}
+		
+        $currMenuBtn.addClass('side-btn');
+
+        var $currentListElement =  $listElement.clone();
+        $currentListElement.append($currMenuBtn);
+
+        $sideMenuList.append($currentListElement);
+    }
+
+    $sideMenuDiv.append($sideMenuList);
+    return $sideMenuDiv;
+}
+
+function getEventObject(ev, array){
+	var $target = $(ev.target);
+	var $content = $(this).next();
+	$content.remove();
+
+	if ($target.hasClass('side-btn')){
+		var dataInfo = $target.attr('data-info');
+		dataInfo = parseInt(dataInfo);
+		console.log('reached; data info: ' + dataInfo);
+		$('#wrapper').append(createEventContent(array[dataInfo]));
+	}
+}
+
+function createEventContent(disaster) {
+	var div = $('<div />').addClass('info-container');
+
+	var eventTitile = $('<h2 />').html(disaster.eventType).addClass('disaster-title');
+	div.append(eventTitile);
+
+        // content
+	for (var i = 0; i < disaster.eventInfo.length; i += 1) {
+		var article = $('<article />').addClass('disaster-article');
+		var infoTitile = $('<h3 />').html(disaster.eventInfo[i].title).addClass('disaster-title');
+		var infoImg = $('<img />').attr('src', disaster.eventInfo[i].imageSource);
+		//vat infoText = [];
+
+		article.append(infoTitile);
+		article.append(infoImg);
+		for (var j = 0; j < disaster.eventInfo[i].text.length; j++) {
+			article.append($('<p/>').text(disaster.eventInfo[i].text[j]));
+		}
+
+		div.append(article);
+	}
+
+	return div;
+}
+
+function createContent(garbageBin) {
+        var div = $('<div />').addClass('info-container');
+
+        var eventTitile = $('<h2 />').html(garbageBin.name).addClass('garbage-bin-color');
+        div.append(eventTitile);
+
+        // content
+        for (var i = 0; i < garbageBin.content; i += 1) {
+            var article = $('<article />').addClass('garbage-bin-info');
+            var infoText = $('<p />').html(garbageBin.content);
+
+            article.append(infoTitile);
+            article.append(infoImg);
+            article.append(infoText);
+            div.append(article);
+        }
+
+        return div;
+    }
--- scripts/objects.js
+++ scripts/objects.js
@@ -1,11 +1,4 @@
-$(document).ready(function () {
-	console.log('started');
-	$(document).on('click', '#disasterButton', function(){
-		onDisastersBtnClick();
-	});
-	
-})
- var disastersInfoBG = [
+﻿var disastersInfoBG = [
         {
             eventType: 'Пожар',
             eventInfo: [
@@ -29,7 +22,7 @@ $(document).ready(function () {
             eventInfo: [
                 {
                     title: 'Основна информация',
-                    text: '',
+                    text: [''],
                     imageSource: 'images/fire.png'
                 },{
                     title: 'Рискове',
@@ -46,7 +39,7 @@ $(document).ready(function () {
             eventInfo: [
                 {
                     title: 'Основна информация',
-                    text: '',
+                    text: [''],
                     imageSource: 'images/fire.png'
                 },{
                     title: 'Рискове',
@@ -63,7 +56,7 @@ $(document).ready(function () {
             eventInfo: [
                 {
                     title: 'Основна информация',
-                    text: '',
+                    text: [''],
                     imageSource: 'images/fire.png'
                 },{
                     title: 'Рискове',
@@ -80,7 +73,7 @@ $(document).ready(function () {
             eventInfo: [
                 {
                     title: 'Основна информация',
-                    text: '',
+                    text: [''],
                     imageSource: 'images/fire.png'
                 },{
                     title: 'Рискове',
@@ -197,80 +190,20 @@ $(document).ready(function () {
         }
 ];
 
+var garbageBins = [
+        {
+            name: 'blue',
+            content: 'content '
+        },
+        {
+            name: 'yellow',
+            content: ' content content'
+        },
+        {
+            name: 'green',
+            content: ' content content content'
+        }
+    ];
+	
  var $listElement = $('<li />');
  var $button = $('<button>');
-
-    onDisastersBtnClick();
-
-    function onDisastersBtnClick(){
-		console.log('buttonClicked');
-        var $wrapper = $('#wrapper');
-		$wrapper.empty();
-        var $sideMenu = createSideMenu();
-        var $selectedDisaster = createEventContent(disastersInfoBG[0]);
-
-        $sideMenu.on('click', getEventObject);
-        $wrapper.append($sideMenu);
-        $wrapper.append($selectedDisaster);
-    }
-
-    function createSideMenu(){
-        var $sideMenuDiv = $('<div/ >');
-        $sideMenuDiv.addClass('side-nav');
-
-        var $sideMenuList = $('<ul />');
-        $sideMenuList.addClass('nav-list');
-
-        for (var i = 0; i < disastersInfoBG.length; i++) {
-            var $currMenuBtn = $button.clone();
-            $currMenuBtn.attr('data-info', i);
-            $currMenuBtn.text(disastersInfoBG[i].eventType);
-            $currMenuBtn.addClass('side-btn');
-
-            var $currentListElement =  $listElement.clone();
-            $currentListElement.append($currMenuBtn);
-
-            $sideMenuList.append($currentListElement);
-        }
-
-        $sideMenuDiv.append($sideMenuList);
-        return $sideMenuDiv;
-    }
-
-    function getEventObject(ev){
-        var $target = $(ev.target);
-        var $content = $(this).next();
-        $content.remove();
-
-        if ($target.hasClass('side-btn')){
-            var dataInfo = $target.attr('data-info');
-            dataInfo = parseInt(dataInfo);
-
-            $('#wrapper').append(createEventContent(disastersInfoBG[dataInfo]));
-        }
-    }
-
-    function createEventContent(disaster) {
-        var div = $('<div />').addClass('info-container');
-
-        var eventTitile = $('<h2 />').html(disaster.eventType).addClass('disaster-title');
-        div.append(eventTitile);
-
-        // content
-        for (var i = 0; i < disaster.eventInfo.length; i += 1) {
-            var article = $('<article />').addClass('disaster-article');
-            var infoTitile = $('<h3 />').html(disaster.eventInfo[i].title).addClass('disaster-title');
-            var infoImg = $('<img />').attr('src', disaster.eventInfo[i].imageSource);
-            //vat infoText = [];
-
-            article.append(infoTitile);
-            article.append(infoImg);
-            for (var j = 0; j < disaster.eventInfo[i].text.length; j++) {
-                article.append($('<p/>').text(disaster.eventInfo[i].text[j]));
-            }
-
-            div.append(article);
-        }
-
-        return div;
-    }
