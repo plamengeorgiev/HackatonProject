b2ed85843e36d8665738221f8523aa85b8309953
plamengeorgiev <plamen.flame@gmail.com>
2014-06-21 14:09:42 +0300

    script file changes
    script file changes

--- scripts/scriptPlamen.js
+++ scripts/scriptPlamen.js
@@ -1,8 +1,8 @@
-function onRecycleButtonClick(){
-    var $container = $('#wrapper');
+$(document).ready(function(){
+console.log('started');
+$(document).on('click', '#recycle', onRecycleBtnClick);
 
-    var $sideMenu = document.createElement('div');
-    var $infoContainer = document.createElement('div');
+});
     var garbageBins = [
         {
             name: 'blue',
@@ -18,6 +18,77 @@ function onRecycleButtonClick(){
         }
     ]
 
+<<<<<<< HEAD
+    var $listElement = $('<li />');
+    var $button = $('<button>');
+
+    function onRecycleBtnClick(){
+        var $wrapper = $('#wrapper');
+		$wrapper.empty();
+        var $sideMenu = createSideMenu();
+        var $selectedDisaster = createEventContent(garbageBins[0]);
+
+        $sideMenu.on('click', getEventObject);
+        $wrapper.append($sideMenu);
+        $wrapper.append($selectedDisaster);
+    }
+
+    function createSideMenu(){
+        var $sideMenuDiv = $('<div/ >');
+        $sideMenuDiv.addClass('side-nav');
+
+        var $sideMenuList = $('<ul />');
+        $sideMenuList.addClass('nav-list');
+
+        for (var i = 0; i < garbageBins.length; i++) {
+            var $currMenuBtn = $button.clone();
+            $currMenuBtn.attr('data-info', i);
+            $currMenuBtn.text(garbageBins[i].eventType);
+            $currMenuBtn.addClass('side-btn');
+
+            var $currentListElement =  $listElement.clone();
+            $currentListElement.append($currMenuBtn);
+
+            $sideMenuList.append($currentListElement);
+        }
+
+        $sideMenuDiv.append($sideMenuList);
+        return $sideMenuDiv;
+    }
+
+    function getEventObject(ev){
+        var $target = $(ev.target);
+
+        if ($target.hasClass('side-btn')){
+            var dataInfo = $target.attr('data-info');
+            dataInfo = parseInt(dataInfo);
+            console.log(dataInfo);
+            createEventContent(garbageBins[dataInfo]);
+        }
+    }
+
+    function createEventContent(garbageBin) {
+        var div = $('<div />').addClass('info-container');
+
+        var eventTitile = $('<h2 />').html(garbageBin.name).addClass('garbage-bin-color');
+        div.append(eventTitile);
+
+        // content
+        for (var i = 0; i < garbageBin.content; i += 1) {
+            var article = $('<article />').addClass('garbage-bin-info');
+            var infoTitile = $('<h3 />').html(disaster.eventInfo[i].title).addClass('garbage-bin-color');
+            var infoText = $('<p />').html(disaster.eventInfo[i].text);
+
+            article.append(infoTitile);
+            article.append(infoImg);
+            article.append(infoText);
+            div.append(article);
+        }
+
+        return div;
+    }
+
+=======
     for (i=0; i<garbageBins.length; i++)
     {
         var button = document.createElement('button');
@@ -35,3 +106,4 @@ function onRecycleButtonClick(){
     $container.add($infoContainer);
 }
 $('#recycle').click(onRecycleButtonClick());
+>>>>>>> origin/master
