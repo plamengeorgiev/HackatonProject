86e0689628b0e2bd1859829be680288adfb39f71
Gencho <Rasilev>
2014-06-22 08:58:01 +0300

    Move script from html to js file

--- scripts/about-as.js
+++ scripts/about-as.js
@@ -1,3 +1,32 @@
 ﻿(function () {
-    
-});
+    var $container = $('#wrapper');
+
+    var $aboutAsBtn = $('#about-as');
+    var $title = $('<h2 />').text('Това е приложение за получаване на известия за настъпили или предстоящи бедствия.');
+    var $image = $('<img />').attr('src', 'images/navodnenie.jpg');
+    $image.addClass('img');
+
+    var $content = $('<div />').addClass('phone');
+    $('<h3 />').text('Спешни телефони:').appendTo($content);
+
+    var phones = [
+        '112  Единен телефон за спешни повиквания',
+        '150  Бърза помощ',
+        '166  Полиция',
+        '160  Пожарна',
+        '146  Пътна помощ'
+    ];
+
+    for (var i = 0; i < phones.length; i++) {
+        var p = $('<p />').text(phones[i]);
+
+        $content.append(p);
+    }
+
+    $aboutAsBtn.on('click', function () {
+        $container.empty();
+        $container.append($title);
+        $container.append($image);
+        $container.append($content);
+    });
+})();
